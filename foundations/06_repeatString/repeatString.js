fc692d3902f43f15abec65ec7bdd21cd69de3957
David Julian <davidjoshuajulian8@gmail.com>
2026-03-06 15:06:35 +0800

    added solution for 06 and 07

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
+const repeatString = function(words, times) {
+
+   if (times < 0) return 'ERROR';
+   let result = "";
+   for (let i = 0; i < times; i++){
+    result += words;
+   }
+   return result;
 
 };
+
+console.log(repeatString('hey', 0));  
 
 // Do not edit below this line
 module.exports = repeatString;
--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,20 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+
+    const words = string.split(' ');
+
+    const reversedWords = words.map(word => {
+
+        return word.split('').reverse().join('');
+        
+    });
+
+    const output = reversedWords.join(' ');
+
+    return output;
 };
+
+console.log(reverseString('Hello World'));
 
 // Do not edit below this line
 module.exports = reverseString;
