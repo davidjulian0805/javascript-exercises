6719fe434705fef8e4af150c99d77499504a4586
David Julian <davidjoshuajulian8@gmail.com>
2026-03-06 15:13:11 +0800

    added solution for 08

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...removeFromArray) {
+    return array.filter(item => !removeFromArray.includes(item));
 };
 
+const storage = [1, 2, 3, 4];
+const result = removeFromArray(storage, 1, 3);
+
+
+console.log(result);
 // Do not edit below this line
 module.exports = removeFromArray;
